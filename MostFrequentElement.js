1be63b8c4ea0b2ee5920134577227e87d8338b08
komlatha <garimidikomalatha62@gmail.com>
2023-07-10 10:48:40 +0530

    A small change was done

--- MostFrequentElement.js
+++ MostFrequentElement.js
@@ -1,10 +1,9 @@
-
 /*17)Write a program to find the Most Frequent Element in an given array.
 let arr = [40, 50, 30, 40, 50, 30, 30];
 let n = arr.length;
 console.log(mostFrequent(arr, n)); //30*/
 let arr = [40, 50, 30, 40, 50, 30, 30,40,60,40,60,20,40,30,50,70,90];
-map =new Map();
+let map =new Map();
 arr.forEach(element => {
     if(map.has(element)){
         map.set(element,map.get(element)+1);
