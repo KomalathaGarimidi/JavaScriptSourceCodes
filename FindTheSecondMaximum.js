1b26f599ed7464fff84d7e85f9c71f12c9a43d4b
komlatha <garimidikomalatha62@gmail.com>
2023-07-10 11:06:19 +0530

    Comments were added

--- FindTheSecondMaximum.js
+++ FindTheSecondMaximum.js
@@ -1,7 +1,7 @@
 // Find the second maximum number in the given array.
 let array=[26,24,1,2,4,5,6,23,4,7,6,9];
-let Fmax=-Infinity;
-let Smax=-Infinity;
+let Fmax=-Infinity;// Infinity is a keyword in javascript ie means no other value is greater than that number
+let Smax=-Infinity;//Here -Infinity means this is the very smallest number 
 for(var i=0;i<array.length;i++){
     if(array[i]>Fmax){
         Smax=Fmax;
